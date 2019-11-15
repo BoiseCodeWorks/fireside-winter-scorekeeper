105ac9f1cd70e9e5f619c38b610f084fbfc174c1
Mark Ohnsman <Mark.Ohnsman@gmail.com>
2019-11-14 18:09:28 -0700

    created initial look, assigning variables

---
+++ app.js
@@ -0,0 +1,14 @@
+let player1 = {
+  score: 0
+};
+
+let player2 = {
+  score: 0
+};
+
+let player = [player1, player2];
+
+let finalscore = 11;
+
+let player1display = document.querySelector("#player1score");
+let player2display = document.querySelector("#player2score");
